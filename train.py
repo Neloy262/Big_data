949c0ca30c2bb3e033cffd4e81927795b31cfcc7
Neloy262 <neloysarwar@gmail.com>
2025-01-23 21:36:19 +0600

    added metrics

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 import lightning as L
+from torchmetrics import Metric
+import torchmetrics
 
 DEFAULT_TRANSFORM = transforms.Compose([
     transforms.ToTensor(),
@@ -15,15 +17,58 @@ class LitClassification(L.LightningModule):
     def __init__(self):
         super().__init__()
         self.model = create_model('resnet34', num_classes=5)
-        self.loss_fn = torch.nn.CrossEntropyLoss()
+
+        total_samples = 25810 + 5292 + 2443 + 873 + 708
+        class_weights = total_samples / (5 * torch.tensor([25810, 5292, 2443, 873, 708], dtype=torch.float))
+        # Normalized weights
+        class_weights = class_weights / class_weights.min()
+
+        self.loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
+        self.accuracy = torchmetrics.Accuracy(task="multiclass",num_classes=5)
+        self.f1_score = torchmetrics.F1Score(task="multiclass",num_classes=5)
 
     def training_step(self, batch):
         images, targets = batch
         outputs = self.model(images)
         loss = self.loss_fn(outputs, targets)
-        self.log("train_loss", loss)
-        return loss
 
+        accuracy = self.accuracy(outputs,targets)
+        f1_score = self.f1_score(outputs,targets)
+
+        self.log_dict({"train_loss":loss,"train_accuracy":accuracy,"train_f1_score":f1_score},on_step=False,on_epoch=True,prog_bar=True,logger=True)
+
+        # self.log("train_loss", loss)
+        return loss
+    
+    def validation_step(self,batch):
+        images, targets = batch
+        outputs = self.model(images)
+        loss = self.loss_fn(outputs, targets)
+
+        accuracy = self.accuracy(outputs,targets)
+        f1_score = self.f1_score(outputs,targets)
+
+        self.log_dict({"val_loss":loss,"val_accuracy":accuracy,"val_f1_score":f1_score},on_step=False,on_epoch=True,prog_bar=True,logger=True)
+
+
+        # self.log("val_loss", loss)
+        return loss
+    
+    def test_step(self,batch):
+        images, targets = batch
+        outputs = self.model(images)
+        loss = self.loss_fn(outputs, targets)
+
+        accuracy = self.accuracy(outputs,targets)
+        f1_score = self.f1_score(outputs,targets)
+
+        self.log_dict({"test_loss":loss,"test_accuracy":accuracy,"test_f1_score":f1_score},on_step=False,on_epoch=True,prog_bar=True,logger=True)
+
+
+        # self.log("test_loss", loss)
+        return loss
+    
+    
     def configure_optimizers(self):
         return torch.optim.AdamW(self.model.parameters(), lr=0.005)
     
@@ -40,10 +85,10 @@ if __name__ == "__main__":
     model = LitClassification()
 
     data = ClassificationData()
-    train_dataset = data.train_dataset("/home/nexus/data_split/train")
-    train_dataloader = DataLoader(train_dataset,batch_size=256,shuffle=True,num_workers=4)
-    val_dataset = data.train_dataset("/home/nexus/data_split/val")
-    val_dataloader = DataLoader(train_dataset,batch_size=256,shuffle=True,num_workers=4)
+    train_dataset = data.train_dataset("/home/mahmud/Downloads/retinopathy_dataset/data_split/train")
+    train_dataloader = DataLoader(train_dataset,batch_size=64,shuffle=True,num_workers=8)
+    val_dataset = data.train_dataset("/home/mahmud/Downloads/retinopathy_dataset/data_split/val")
+    val_dataloader = DataLoader(train_dataset,batch_size=64,shuffle=False,num_workers=8)
     
     trainer = L.Trainer(max_epochs=20)
     trainer.fit(model, train_dataloaders=train_dataloader,val_dataloaders=val_dataloader)
